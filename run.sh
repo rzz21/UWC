618ff2adb6c4ac13ab714b77cc7d95b81428453b
rzz21 <rzz21@mails.tsinghua.edu.cn>
2025-10-27 14:47:47 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 120 --epochs 150 --seed 2026 -a 0 -n 125
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 50 --epochs 150 --seed 2025 -a 0 -n 125
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 50 --epochs 150 --seed 2026 -a 0 -n 125
-# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2025 -a 0 -n 125
-# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2026 -a 0 -n 125
+python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2025 -a 0 -n 125
+python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2026 -a 0 -n 125
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 150 --epochs 150 --seed 2025 -a 0 -n 125
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 150 --epochs 150 --seed 2026 -a 0 -n 125
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 200 --epochs 150 --seed 2025 -a 0 -n 125
@@ -19,10 +19,10 @@
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 200 --epochs 150 --seed 2102 -a 0 -n 125
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 150 --epochs 150 --seed 2102 -a 0 -n 125
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 120 --epochs 150 --seed 2102 -a 0 -n 125
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 80 --epochs 150 --seed 2102 -a 0 -n 125
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 50 --epochs 150 --seed 2102 -a 0 -n 125
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 30 --epochs 150 --seed 2102 -a 0 -n 125
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 80 --epochs 150 --seed 2102 -a 0 -n 125
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 50 --epochs 150 --seed 2102 -a 0 -n 125
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 30 --epochs 150 --seed 2102 -a 0 -n 125
 
 # 120 km ~ munk_A / munk_B / munk_C
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 120 --epochs 150 --seed 2025 -a 0 -n 125 -s munk_B
@@ -41,30 +41,32 @@ python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 3
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 120 --epochs 50 --seed 2026 -a 0 -n 25 -s munk_C
 
 # 100 km ~ munk_A / munk_B / munk_C
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2025 -a 0 -n 125 -s munk_B
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2026 -a 0 -n 125 -s munk_B
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125 -s munk_B
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2025 -a 0 -n 125 -s munk_C
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2026 -a 0 -n 125 -s munk_C
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125 -s munk_C
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2025 -a 0 -n 125 -s munk_B
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2026 -a 0 -n 125 -s munk_B
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125 -s munk_B
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2025 -a 0 -n 125 -s munk_C
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2026 -a 0 -n 125 -s munk_C
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125 -s munk_C
 
 # 100 km ~ munk_A -> munk_B / munk_C ~ finetune
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2026_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2025_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2026_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2025_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -s munk_B
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -s munk_B
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -s munk_B
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -s munk_C
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -s munk_C
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -s munk_C
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2026_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2025_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2026_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2025_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -s munk_B
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -s munk_B
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -s munk_B
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -s munk_C
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -s munk_C
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2102 -a 0 -n 25 -s munk_C
 
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -f --finetune-scenario munk_B --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2025 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
 # python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 50 --seed 2026 -a 0 -n 25 -f --finetune-scenario munk_C --finetune-range 100 --pretrained /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/checkpoints_munk_A_range100km/seed_2102_trainvalnum_125_testnum_500_epoch_150_finetuneFalse_alpha_0.0_schedulercosine/last.pth
 
-python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125 --bty guassian
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2025 -a 0 -n 125
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2026 -a 0 -n 125
+# python /home/zhizhen/UWC/SPPNet_finetune_free_nr_ig_scale_mask/main.py --range 100 --epochs 150 --seed 2102 -a 0 -n 125
